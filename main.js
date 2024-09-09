039bae14e1021b49e08335fe7a1a9a4a76dcf3c6
kaizz04 <kaveesh@marketpushapps.com>
2024-09-10 00:47:46 +0530

    Convert Functions to Arrow Functions

---
+++ main.js
@@ -0,0 +1,13 @@
+// Arrow function
+
+// function greet(){
+//     return `Hello, ${n}`;
+// }
+let n = "kaizz";
+
+
+let greet = ()=> `Hello ${n}`;
+console.log(greet());
+
+const multiply = (a,b)=> a * b;
+console.log(multiply(4,5));
