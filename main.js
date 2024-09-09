882b29889de3fd36b7c19c750015e9f6607f98f5
kaizz04 <kaveesh@marketpushapps.com>
2024-09-10 00:58:44 +0530

    Use Arrow Functions in map Method

--- main.js
+++ main.js
@@ -7,7 +7,11 @@ let n = "kaizz";
 
 
 let greet = ()=> `Hello ${n}`;
-console.log(greet());
+// console.log(greet());
 
 const multiply = (a,b)=> a * b;
-console.log(multiply(4,5));
+// console.log(multiply(4,5));
+
+const arr = [1,2,3,4,5];
+const doubleArr =  arr.map(n => n*2);
+console.log(doubleArr);
